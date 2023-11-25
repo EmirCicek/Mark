0c723bfbf810f73bdbf5115f2be9a1758c7d5460
Emir ÇİÇEK <142392017+EmirCicek@users.noreply.github.com>
2023-11-25 13:55:38 +0300

    Adding the clock feature

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
 import pyautogui
 from responsive_voice import ResponsiveVoice
 import speech_recognition as sr
+from datetime import datetime
+
 
 ENGINE = ResponsiveVoice()
 ENGINE = ResponsiveVoice(lang=ResponsiveVoice.TURKISH)
 
-def sesi_metne_cevir():
+def speech_to_text():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
@@ -14,8 +16,8 @@ def sesi_metne_cevir():
         ses = recognizer.listen(source, timeout=5)
 
     try:
-        metin = recognizer.recognize_google(ses, language="tr-TR")
-        return metin
+        text = recognizer.recognize_google(ses, language="tr-TR")
+        return text
     except sr.UnknownValueError:
         print("Ses algılanamadı.")
     except sr.RequestError as e:
@@ -45,6 +47,10 @@ def refresh():
     pyautogui.leftClick(50,100) # Clicking the browser for refresh
     pyautogui.hotkey('ctrl','f5') # Refreshing the browser
 
+def full_screen():
+    pyautogui.leftClick(50,100)
+    pyautogui.hotkey('f11')
+
 def open_weather_website():
     update_file('source.txt','script.js','hava-durumu-js-start','hava-durumu-js-end')
     update_file('source.txt','index.html','hava-durumu-html-start','hava-durumu-html-end')
@@ -54,4 +60,32 @@ def open_weather_website():
 def text_to_speech(text):
     print(text)
     ENGINE.say(text.encode('utf-8'))
+
+
+def time():
+    days = {
+        'Monday' : 'Pazartesi',
+        'Tuesday' : 'Salı',
+        'Wednesday' : 'Çarşamba',
+        'Thursday' : 'Perşembe',
+        'Friday' : 'Cuma',
+        'Saturday' : 'Cumartesi',
+        'Sunday' : 'Pazar'
+    }
+    
+    now = datetime.now()
+    hour = now.strftime("%H:%M")
+    # date = now.strftime("%Y-%m-%d")
+    date = now.strftime("%d-%m-%Y")
+    day = now.strftime("%A")
+
+    result = [hour,date,days[day]]
+
+    return result # [0] --> 12:38 [1] --> 25-11-2023 [2] --> Saturday(Cumartesi)
+
+
+def open_time():
+    update_file('source.txt','index.html','saat-html-start','saat-html-end')
+    update_file('source.txt','script.js','saat-js-start','saat-js-end')
+    refresh()
 
--- main.py
+++ main.py
@@ -3,23 +3,30 @@ import functions
 
 while True:
     try:
-        tetikleyici_komut = str(functions.sesi_metne_cevir())
-        if 'Mark'.lower() in tetikleyici_komut.lower() or 'Mark 1'.lower() in tetikleyici_komut.lower() or 'Hey Mark'.lower() in tetikleyici_komut.lower() or 'Hey Mark 1'.lower() in tetikleyici_komut.lower(): 
-            print('Input :',tetikleyici_komut)
+        trigger_command = str(functions.speech_to_text())
+        if 'Mark'.lower() in trigger_command.lower() or 'Mark 1'.lower() in trigger_command.lower() or 'Hey Mark'.lower() in trigger_command.lower() or 'Hey Mark 1'.lower() in trigger_command.lower(): 
+            print('Input :',trigger_command)
             functions.text_to_speech('Sizi Dinliyorum')
 
-            try: # Program tetiklendikten sonra komut alma
-                komut = str(functions.sesi_metne_cevir())
-                if 'hava durumu' in komut.lower() or 'hava durumunu aç' in komut.lower():
+            try:
+                command = str(functions.speech_to_text())
+                if 'hava durumu' in command.lower() or 'hava durumunu aç' in command.lower():
                     functions.open_weather_website()
                     functions.text_to_speech('Hava Durumu Açıldı')
-                elif 'beyaz ekran' in komut.lower():
+                elif 'beyaz ekran' in command.lower():
                     functions.update_file('source.txt','index.html','beyaz-ekran-html-start','beyaz-ekran-html-end')
                     functions.refresh()
                     functions.text_to_speech('Beyaz Ekran Açıldı')
-                elif 'programı sonlandır' in tetikleyici_komut.lower():
+                elif 'saat' in command.lower():
+                    functions.open_time()
+                    functions.text_to_speech('Saat '+ functions.time()[0])
+                elif 'tam ekran' in command.lower() or 'ekran modu' in command.lower()  :
+                    functions.full_screen()
+                elif 'programı sonlandır' in trigger_command.lower():
                     functions.text_to_speech('Program Sonlandırılıyor')
                     break
+                else:
+                    print(command)
             except:
                 print('Komut Almaya Çalışılırken Bir Hata Oluştu')
 
